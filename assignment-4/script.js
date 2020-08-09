1a5bb4ad7fab7455cc0753eedd5b828a59b9fbab
Ashrfq <59022706+Ashrfq@users.noreply.github.com>
2020-08-09 10:52:19 +0530

    Update script.js

--- assignment-4/script.js
+++ assignment-4/script.js
@@ -40,7 +40,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i in names) {
-
+console.log(names[i]);
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
